eef878d474312da663296c621bb7ae26d86f5ee4
unknown <53538316+grlucy@users.noreply.github.com>
2020-01-29 15:45:13 -0500

    added GET for /api/notes

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(express.json());
 
 // -----------------------------------------------------
 // Data
-//
-//
+
+const database = require("./Develop/db/db.json");
 
 // -----------------------------------------------------
 // Routes
@@ -30,19 +30,21 @@ app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
 });
 
+// GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON
+
+app.get("/api/notes", function(req, res) {
+  return res.json(database);
+});
+
+// POST `/api/notes` - Should recieve a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client
+
+// DELETE `/api/notes/:id` - Should recieve a query paramter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file
+
 // GET `*` - Should return the `index.html` file
 
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
 });
-
-// The application should have a `db.json` file on the backend that will be used to store and retrieve notes using the `fs` module.
-
-// GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON
-
-// POST `/api/notes` - Should recieve a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client
-
-// DELETE `/api/notes/:id` - Should recieve a query paramter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file
 
 // -----------------------------------------------------
 // Listener
