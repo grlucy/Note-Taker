9284e2c6762355e1f0d31f117ae44e6eb2121bda
unknown <53538316+grlucy@users.noreply.github.com>
2020-01-29 16:37:02 -0500

    added POST for /api/notes and implemented express.static

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const util = require("util");
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
+const appendFileAsync = util.promisify(fs.appendFile);
 
 // -----------------------------------------------------
 // Express app
@@ -24,6 +25,11 @@ const database = require("./Develop/db/db.json");
 // -----------------------------------------------------
 // Routes
 
+// GET `*` - Should return the `index.html` file
+// ---- DON'T NEED A GET FOR INDEX.HTML SINCE USING EXPRESS.STATIC ----
+
+app.use(express.static("Develop/public"));
+
 // GET `/notes` - Should return the `notes.html` file
 
 app.get("/notes", function(req, res) {
@@ -36,15 +42,25 @@ app.get("/api/notes", function(req, res) {
   return res.json(database);
 });
 
-// POST `/api/notes` - Should recieve a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client
+// POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client
+
+app.post("/api/notes", function(req, res) {
+  const newNote = req.body;
+  console.log(newNote);
+  let maxID = 0;
+  for (const note of database) {
+    let currentID = note.id;
+    if (currentID > maxID) {
+      maxID = currentID;
+    }
+  }
+  newNote.id = maxID + 1;
+  console.log(newNote);
+  database.push(newNote);
+  res.json(newNote);
+});
 
 // DELETE `/api/notes/:id` - Should recieve a query paramter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file
-
-// GET `*` - Should return the `index.html` file
-
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
-});
 
 // -----------------------------------------------------
 // Listener
